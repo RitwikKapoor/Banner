4b157320d9e1756b3b8536d09e57ec358f67d3bb
Ritwik <kapoorritwik2020@gmail.com>
2024-08-12 15:14:57 +0530

    changes for deployment

--- server/middlewares/jwtCheck.js
+++ server/middlewares/jwtCheck.js
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const jwtCheck = (req, res, next) => {
-  console.log("Inside jwtCheck")
   const token = req.header("Authorization")?.split(" ")[1];
-  console.log("token val = ", token)
   if (!token) {
     return res.status(401).json({ error: "Access denied, no token provided." });
   }
